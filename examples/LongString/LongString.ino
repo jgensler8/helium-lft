473a663abfdaad6b0f2d4a2178ec0308705a878c
Jeffrey Gensler <jeffrey.gensler@cdk.com>
2018-05-13 19:55:42 -0500

    add examples directory with similar code as the helium library

---
+++ examples/LongString/LongString.ino
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2017, Helium Systems, Inc.
+ * All Rights Reserved. See LICENCE.txt for license information
+ */
+
+#include "Arduino.h"
+#include "Board.h"
+#include <Helium.h>
+#include <HeliumUtil.h>
+#include <heliumlft.h>
+
+// NOTE: Please ensure you've created a channel with the above
+// CHANNEL_NAME as it's name.
+#define CHANNEL_NAME "Helium MQTT"
+
+Helium  helium(&atom_serial);
+Channel channel(&helium);
+
+void
+setup()
+{
+    Serial.begin(9600);
+    DBG_PRINTLN(F("Starting"));
+
+    // Begin communication with the Helium Atom
+    // The baud rate differs per supported board
+    // and is configured in Board.h
+    helium.begin(HELIUM_BAUD_RATE);
+
+    // Connect the Atom to the Helium Network
+    helium_connect(&helium);
+
+    // Begin communicating with the channel. This should only need to
+    // be done once. The HeliumUtil functions add simple retry logic
+    // to re-create a channel if it disconnects.
+    channel_create(&channel, CHANNEL_NAME);
+}
+
+void
+loop()
+{
+    const char * data = "part1.....20........30........40........50........60........70........80........90........100part2........120.......130.......\n";
+    int len = strlen(data);
+
+    // Send data to channel
+    channel_send_lft(&channel, CHANNEL_NAME, data, strlen(data));
+
+    // Wait about 5 seconds
+    delay(5000);
+}
